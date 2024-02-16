a9d1d5d8291e0b07390818cdadedb47c184fe147
Luis Ayala <ayalaluis638@gmail.com>
2024-02-15 22:54:18 -0300

    Proyecto final

---
+++ index.js
@@ -0,0 +1,11 @@
+function PlayAudio() {
+    document.getElementById("musica").play();
+}
+
+const toggleBtn = document.querySelector('.toggle_btn');
+const toggleBtnIcon = document.querySelector('.toggle_btn i');
+const dropDownMenu = document.querySelector('.dropdown_menu');
+
+toggleBtn.onclick = function() {
+    dropDownMenu.classList.toggle('open')
+}
